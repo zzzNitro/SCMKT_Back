faf1dcf8142e2042e74f23da9406cb4008e0800e
Nitro <pablo.benito@rocketmail.com>
2021-11-12 19:27:10 -0300

    Contract routes getAll and getById working

--- src/routes/contract.js
+++ src/routes/contract.js
@@ -7,31 +7,26 @@ const { Op } = require('sequelize');
 
 router.get('/', async function (req, res, next) {
   try {
-    let found = await Contract.findAll({
-      include: {
-        model: User,
-        attributes: ['name', 'last_name'],
-      }
-    })
+    let found = await Contract.findAll()
 
     let contracts = found.map((el) => {
       let contract = {
-        id: found.id,
-        wallet1: found.wallet1,
-        wallet2: found.wallet2,
-        conditions: found.conditions,
-        status: found.status
+        id: el.id,
+        wallet1: el.wallet1,
+        wallet2: el.wallet2,
+        conditions: el.conditions,
+        status: el.status
       }
       return contract;
     })
 
-    return res.json([...contracts]);
-  } catch {
+    return res.json([...contracts])
+   } catch {
     return res.next({ message: 'Some Error' })
   }
 });
 
-/* 
+
 router.get('/:id', async function (req, res, next) {
   const { id } = req.params;
 
@@ -46,12 +41,12 @@ router.get('/:id', async function (req, res, next) {
       status: found.status
     }
 
-    return res.json(user);
+    return res.json(contract);
   } catch (error) {
     return next({ message: error })
   }
 });
-*/
+
 
 
 module.exports = router;
