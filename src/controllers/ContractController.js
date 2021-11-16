3219d0b119516f4efe01de97e5d9c4ba2eb5b316
Nitro <pablo.benito@rocketmail.com>
2021-11-16 14:50:19 -0300

    Minor fixes and script for heroku deploy

--- src/controllers/ContractController.js
+++ src/controllers/ContractController.js
@@ -25,103 +25,103 @@ async function GetContracts (req, res, next) {
     }
   };
 
-  async function GetContractById (req, res, next) {
-    const { id } = req.params;
-  
-    try {
-      let found = await Contract.findByPk(id, { include: [User] })
-  
-      let contract = {
-        id: found.id,
-        wallet1: found.wallet1,
-        wallet2: found.wallet2,
-        conditions: found.conditions,
-        status: found.status
-      }
-  
-      return res.json(contract);
-    } catch (error) {
-      return next({ message: error })
-    }
-  };
+async function GetContractById (req, res, next) {
+  const { id } = req.params;
 
-  async function EditContract (req, res, next){
-    const id = req.params.id;
-    const { conditions, status } = req.body;
-    
+  try {
+    let found = await Contract.findByPk(id, { include: [User] })
 
-    try {
-      let found = await Contract.findByPk(id)
-      let contract = {
-        conditions: {
-          name: found.conditions.name || "not defined",
-          shortdescription: found.conditions.shortdescription || "not defined",
-          longdescription: found.conditions.longdescription || "not defined",
-          amount: found.conditions.amount || "not defined",
-          coin: found.conditions.amount || "not defined",
-          condition: {
-            c1: found.conditions.condition.c1 || "not defined",
-            c1: found.conditions.condition.c2 || "not defined"
-          }
-        },
-        status: found.status || "unpublished"
-      }
-
-      let updatedContract = await Contract.update({
-        conditions: {
-          name: `${ conditions.name ? conditions.name : contract.conditions.name }`,
-          shortdescription: `${ conditions.shortdescription ? conditions.shortdescription : contract.conditions.shortdescription }`,
-          longdescription: `${ conditions.longdescription ? conditions.longdescription : contract.conditions.longdescription }`,
-          amount: `${ conditions.amount ? conditions.amount : contract.conditions.amount }`,
-          coin: `${ conditions.coin ? conditions.coin : contract.conditions.coin }`,
-          condition: {
-            c1: `${ conditions.condition.c1 ? conditions.condition.c1 : contract.conditions.condition.c1 }`,
-            c2: `${ conditions.condition.c2 ? conditions.condition.c2 : contract.conditions.condition.c2 }`
-          }
-        },
-        status: `${ status ? status : contract.status }`,
-        },
-        {where: {id}}
-      )
-      res.sendStatus(200)
-    } catch(error){
-      res.send(error)
-    }
-  };
-
-  async function DeleteContract (req, res, next){
-    const id = req.params.id;
-  
-    try {
-      await Contract.update(
-        {status: "deleted"},
-        {where: {id}}
-      )
-      return res.sendStatus(200)
-    } catch(error){
-      res.send(error)
-    }
-  }
-
-  async function NewContract (req, res) {
-    const {wallet1, wallet2, conditions, status} = req.body;
     let contract = {
-      id: uuidv4(),
-      wallet1,
-      wallet2,
-      conditions,
-      status
+      id: found.id,
+      wallet1: found.wallet1,
+      wallet2: found.wallet2,
+      conditions: found.conditions,
+      status: found.status
     }
-    
-    await Contract.create(contract);
 
-    res.json(contract)
+    return res.json(contract);
+  } catch (error) {
+    return next({ message: error })
+  }
+};
+
+async function EditContract (req, res, next){
+  const id = req.params.id;
+  const { conditions, status } = req.body;
+  
+
+  try {
+    let found = await Contract.findByPk(id)
+    let contract = {
+      conditions: {
+        name: found.conditions.name || "not defined",
+        shortdescription: found.conditions.shortdescription || "not defined",
+        longdescription: found.conditions.longdescription || "not defined",
+        amount: found.conditions.amount || "not defined",
+        coin: found.conditions.amount || "not defined",
+        condition: {
+          c1: found.conditions.condition.c1 || "not defined",
+          c1: found.conditions.condition.c2 || "not defined"
+        }
+      },
+      status: found.status || "unpublished"
+    }
+
+    let updatedContract = await Contract.update({
+      conditions: {
+        name: `${ conditions.name ? conditions.name : contract.conditions.name }`,
+        shortdescription: `${ conditions.shortdescription ? conditions.shortdescription : contract.conditions.shortdescription }`,
+        longdescription: `${ conditions.longdescription ? conditions.longdescription : contract.conditions.longdescription }`,
+        amount: `${ conditions.amount ? conditions.amount : contract.conditions.amount }`,
+        coin: `${ conditions.coin ? conditions.coin : contract.conditions.coin }`,
+        condition: {
+          c1: `${ conditions.condition.c1 ? conditions.condition.c1 : contract.conditions.condition.c1 }`,
+          c2: `${ conditions.condition.c2 ? conditions.condition.c2 : contract.conditions.condition.c2 }`
+        }
+      },
+      status: `${ status ? status : contract.status }`,
+      },
+      {where: {id}}
+    )
+    res.sendStatus(200)
+  } catch(error){
+    res.send(error)
+  }
+};
+
+async function DeleteContract (req, res, next){
+  const id = req.params.id;
+
+  try {
+    await Contract.update(
+      {status: "deleted"},
+      {where: {id}}
+    )
+    return res.sendStatus(200)
+  } catch(error){
+    res.send(error)
+  }
+}
+
+async function NewContract (req, res) {
+  const {wallet1, wallet2, conditions, status} = req.body;
+  let contract = {
+    id: uuidv4(),
+    wallet1,
+    wallet2,
+    conditions,
+    status
+  }
+  
+  await Contract.create(contract);
+
+  res.json(contract)
+};
+
+module.exports = {
+  GetContracts,
+  GetContractById,
+  EditContract,
+  DeleteContract,
+  NewContract
   };
-
-  module.exports = {
-    GetContracts,
-    GetContractById,
-    EditContract,
-    DeleteContract,
-    NewContract
-    };
