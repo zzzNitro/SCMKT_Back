579fe3ebbf0563c7948403532b6ddaaac4a49ce4
Nitro <pablo.benito@rocketmail.com>
2021-11-17 08:39:22 -0300

    added fix for heroku deploy

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { preCharge } = require('./src/controllers/PreChargeController')
 const axios = require('axios');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
-  server.listen(3001, async () => {
+  server.listen(process.env.PORT || 3001, async () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
