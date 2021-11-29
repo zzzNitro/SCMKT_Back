c0a302a3a6303267698fac9850165ad6ee7ff56e
Nitro <pablo.benito@rocketmail.com>
2021-11-28 22:13:21 -0300

    changed setHeader for header on app.js

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ server.use(function (req, res, next) {
   var allowedDomains = ['http://localhost:3001','https://henryfront.surge.sh/' ];
   var origin = req.headers.origin;
   if(allowedDomains.indexOf(origin) > -1){
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
